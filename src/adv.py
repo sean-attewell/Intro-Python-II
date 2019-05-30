b22af8b036612eab34e004989b98a082a7df1b84
sean-attewell <sean.attewell@gmail.com>
2019-05-30 17:31:06 +0100

    MVP

--- src/adv.py
+++ src/adv.py
@@ -165,15 +165,3 @@ while selection != "q":
         clearScreen()
         print(
             "\033[31mInvalid selection, Please choose from the options provided.\033[m\n")
-
-
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
